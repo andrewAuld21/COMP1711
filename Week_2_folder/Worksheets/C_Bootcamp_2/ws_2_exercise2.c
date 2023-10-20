811abc6c3d331a8354082eaaa4c853dc93e99b3c
andrewAuld21 <146656365+andrewAuld21@users.noreply.github.com>
2023-10-20 12:40:49 +0000

    Continued work on C Bootcamp 2

--- Week_2_folder/Worksheets/C_Bootcamp_2/ws_2_exercise2.c
+++ Week_2_folder/Worksheets/C_Bootcamp_2/ws_2_exercise2.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
-    int a [] = {20, 10, 12, 33, 80};
+    int arr[1000];
+    int num;
+
+    //Prompts the user to enter the size of the array
+    printf("Enter the size of the array: ");
+    scanf("%i", &num);
+
+    //Prompts the user to enter the elements in the array
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i <= num; i++)
+    {
+        scanf("%i", &arr[i]);
+    }
+
+    //Now we reverse the array
+    int start = 0;
+    int end = num - 1;
+
+    while(start < end){
+
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    //Prints the reversed array
+    printf("The reversed array is: ");
+    for (int i = 0; i <= num; i++)
+    {
+        printf("%i", arr[i]);
+    }
+
+    return 0;
 
 }

5499f96b0046a265a250b021ce7de9f37254fbdb
andrewAuld21 <146656365+andrewAuld21@users.noreply.github.com>
2023-10-22 22:07:20 +0000

    Started on C Bootcamp Week 2 Worksheet 2

---
+++ Week_3_folder/Worksheets/cbootcampws2ex1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+//Write a C program that defines a function circleArea which takes a float radius as a parameter and returns the area of
+//the circle. Test this function in your main function
+
+float circleArea(){
+
+    

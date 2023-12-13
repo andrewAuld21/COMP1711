aabc9af193603a3b393b0133a25935e3a8b2975e
andrewAuld21 <146656365+andrewAuld21@users.noreply.github.com>
2023-12-13 03:57:16 +0000

    Finished Task 3

--- Coursework/FitnessDataSorter.c
+++ Coursework/FitnessDataSorter.c
@@ -10,56 +10,116 @@ typedef struct {
 } FitnessData;
 
 // Function to tokenize a record
-void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *steps) {
-    char *ptr = strtok(record, &delimiter);
-    if (ptr != NULL) {
-        strcpy(date, ptr);
-        ptr = strtok(NULL, &delimiter);
-        if (ptr != NULL) {
-            strcpy(time, ptr);
-            ptr = strtok(NULL, &delimiter);
-            if (ptr != NULL) {
-                *steps = atoi(ptr);
-            }
-        }
+void tokeniseRecord(const char *input, const char *delimiter, char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {
+        strcpy(date, token);
     }
+
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+
+    // Free the duplicated string
+    free(inputCopy);
 }
 
+// Function to compare FitnessData records for qsort
+// I based this function on a discussion with ChatGPT
+// Prompt: How can I compare items in a list in order to sort them?
+int compareRecords(const void *a, const void *b) 
+{
+    return ((FitnessData *)b)->steps - ((FitnessData *)a)->steps;
+}
 
 int main() {
 
+    // Initializes the size of 'buffer_size' to 100
     int buffer_size = 100;
+    // 'line_storage_array' will store the lines that are read in and it is set to be the same size as 'buffer_size'
     char line_storage_array[buffer_size];
+    // This variable is for storing the name of the file being read in
     char filename[buffer_size];
+    // Initializes the 'counter' variable to 0
     int counter = 0;
+    // This variable is for storing the name of the file that will be output
+    char new_file_filename[buffer_size];
 
+    // Created a new array 'file_info' and assigned it the 'FITNESS_DATA' structure type
     FitnessData file_info[100];
 
+    // Created new arrays to store the 'date', 'time', and 'steps' strings
     char date[11];
     char time[6];
     char steps[100];
 
-    printf("Enter Filename: ");
+    // Asks the user for a filename
+    printf("Enter filename: ");
+    // Gets the input from the user and stores it in line_storage_array
     fgets(line_storage_array, buffer_size, stdin);
+    // Stores string that holds the name of the file into 'filename'
     sscanf(line_storage_array, "%s", filename);
 
+    // Opens the original file
     FILE *input = fopen(filename, "r");
-    if (!input)
+    if (!input) 
     {
+        // Prints an error message if the file is not the correct file
         printf("Error: invalid file.\n");
         return 1;
     }
 
+    // Uses the tokeniseRecord function to separate the strings being read in into date, time and steps
+    // As well as changing file_info[counter].steps into an int value
+    // 'counter++' reads through every line in the file
     while (fgets(line_storage_array, buffer_size, input) != NULL)
     {
-        tokeniseRecord(line_storage_array, ',', date, time, &file_info[counter].steps);
+        tokeniseRecord(line_storage_array, ",", date, time, steps);
         strcpy(file_info[counter].date, date);
         strcpy(file_info[counter].time, time);
-        printf("Original order: ");
-        printf("%s %s %i\n", file_info[counter].date, file_info[counter].time, file_info[counter].steps);
+        file_info[counter].steps = atoi(steps);
         counter++;
     }
 
-    return 0;
+    // Sorts the data in descending order of steps using the 'compareRecords' function
+    qsort(file_info, counter, sizeof(FitnessData), compareRecords);
 
+    // Creates a new filename with .tsv extension
+    snprintf(new_file_filename, buffer_size, "%s.tsv", filename);
+
+    // Opens the new file where the sorted lines will go
+    FILE *output = fopen(new_file_filename, "w");
+    if (!output)
+    {
+        // Prints an error message if the output file is not the correct name
+        printf("Error: could not create file.\n");
+        return 1;
+    }
+
+    // Iterates through every line in the file
+    for (int i = 0; i < counter; i++)
+    {
+        // Prints the date, time and steps of the line into the new file
+        fprintf(output, "%s\t%s\t%d\n", file_info[i].date, file_info[i].time, file_info[i].steps);
+    }
+
+    // Closes the input and output file
+    fclose(input);
+    fclose(output);
+
+    // Prints a message to show the program has been executed successfully
+    printf("Data sorted and written to %s.tsv\n", filename);
+
+    return 0;
+    
 }
--- Coursework/StepCounter_Final.c
+++ Coursework/StepCounter_Final.c
@@ -43,8 +43,11 @@ void tokeniseRecord(const char *input, const char *delimiter,
 // Complete the main function
 int main() {
 
+    // 'buffer_size' is the maximum amount of characters that can be read in per line
    int buffer_size = 100;
+   // This variable is for storing the name of the file being read in
    char filename[buffer_size];
+    // 'line_storage_array' will store the lines that are read in and it is set to be the same size as 'buffer_size'
    char line_storage_array[buffer_size];
 
    char option;
@@ -61,12 +64,15 @@ int main() {
    int longest_period_length;
    
 
+    // Created a new array 'file_info' and assigned it the 'FITNESS_DATA' structure type
    FITNESS_DATA file_info[1000];
 
+    // Created new arrays to store the 'date', 'time', and 'steps' strings
    char date[11];
    char time[6];
    char steps[100];
 
+    // This while loop keeps the menu system running after a choice has been selected
    while(1)
    {
 
@@ -80,19 +86,25 @@ int main() {
         printf("Q: Quit\n");
         printf("Enter choice: ");
 
+        // This line of code gets the character that the user inputs in order to know which case was chosen
         option = getchar();
 
+        // Initiates a while loop that runs as long as the character does not equal the newline character '(\n)'
         while(getchar() != '\n');
 
+        // Initiates the switch statement to choose the menu options
         switch(option)
         {
             case 'A':
             case 'a':
 
+                // Prompts the user to enter the filename and stores the input (stdin) from the user into 'line_storage_array'
+                // And then stores the string in 'line_storage_array' into 'filename'
                 printf("Input filename: ");
                 fgets(line_storage_array, buffer_size, stdin);
                 sscanf(line_storage_array, "%s", filename);
 
+                // Opens the file in 'read' mode and prints an error if the correct file is not selected.
                 FILE *input = fopen(filename, "r");
                 if (!input)
                 {
@@ -100,6 +112,9 @@ int main() {
                     return 1;
                 }
 
+                // Uses the tokeniseRecord function to separate the strings being read in into date, time and steps
+                // As well as changing file_info[counter].steps into an int variable 'steps_as_int'
+                // 'counter++' reads through every line in the file
                 while (fgets(line_storage_array, buffer_size, input) != NULL)
                 {
                     tokeniseRecord(line_storage_array, ",", date, time, steps);
@@ -111,22 +126,28 @@ int main() {
                     counter++;
                 }
 
+                // Makes number of records equal to counter
                 number_of_records = counter;
 
+                // Prints the statement if the file is successfully accessed and loaded
                 printf("File successfully loaded.\n");
 
                 break;
         
             case 'B':
             case 'b':
+                // Prints the numebr of records in the file
                 printf("Total records: %i\n", number_of_records);
                 break;
         
             case 'C':
             case 'c': 
 
+                // Initializes 'least_steps' to 10,000
                 least_steps = 10000;
+                // Iterates through all the lines of the file
                 for(int i = 0; i < number_of_records; i++){
+                    // If the steps are lower than least_steps, update the value of least_steps
                     if(file_info[i].steps < least_steps)
                     {
                         least_steps = file_info[i].steps;
@@ -136,6 +157,7 @@ int main() {
             
                 }
 
+                // Prints the date and time of the least steps value
                 printf("Fewest steps: %s %s\n", file_info[min_steps_index].date, file_info[min_steps_index].time);
 
                 break;
@@ -143,8 +165,11 @@ int main() {
             case 'D':
             case 'd':
 
+                // Initializes 'most_steps' to 0
                 most_steps = 0;
+                //Iterates through all the lines of the file
                 for(int i = 0; i < number_of_records; i++){
+                    // If the steps are greater than 'most_steps', update the value of 'most_steps'
                     if(file_info[i].steps > most_steps)
                     {
                         most_steps = file_info[i].steps;
@@ -153,6 +178,7 @@ int main() {
 
                 }
 
+                // Prints the date and time of the most_steps value
                 printf("Largest steps: %s %s\n", file_info[most_steps_index].date, file_info[most_steps_index].time);
 
                 break;
@@ -160,19 +186,28 @@ int main() {
             case 'E':
             case 'e':
                 
+                // Iterates through all the lines in the file
                 for(int i = 0; i < number_of_records; i++)
+                // Updates the value of 'mean' with every 'steps' value in each line
                 {
                     mean += file_info[i].steps;
                 }
 
+                // Divides the value of 'mean' by the number of lines in the file
                 mean /= counter;
+                // Prints the value of 'mean'
                 printf("Mean step count: %.0f\n", mean);
                 break;
             
             case 'F':
             case 'f':
+
+                // Iterates through all the lines of the file
                 for(int i = 0; i < number_of_records; i++)
                 {
+                    // If the value of file_info[i].steps is greater than 500, it initializes the length of the period to 0
+                    // And assigns the line 'i' to the start of the longest period variable 'current_start_index'
+                    // And continues increasing the value of the length of the period with 'current_period_length++'
                     if(file_info[i].steps > 500)
                     {
                         if(current_period_length == 0)
@@ -183,27 +218,37 @@ int main() {
                         current_period_length++;
 
                     }else{
+                        // If current_period_length is longer than longest_period_length (which at the beginning is 0)
+                        // then it updates the value of longest_period_length to the value of current_period_length
+                        // as well as the value of the starting index of the longest period
                         if(current_period_length > longest_period_length)
                         {
                             longest_period_length = current_period_length;
                             longest_start_index = current_start_index;
                         }
 
+                        // Restarts the value of 'current_period_length' to 0, so that it can accurately find the length
+                        // of another continuous period
                         current_period_length = 0;
 
                     }
                 }     
                 
+                // Prints the date and time of the start of the longest continuous period
                 printf("Longest period start: %s %s\n", file_info[longest_start_index].date, file_info[longest_start_index].time);
-                printf("Longest period end: %s %s\n", file_info[longest_start_index + longest_period_length].date, file_info[longest_start_index + longest_period_length].time);
+
+                // Prints the date and time of the end of the longest period by adding the length to the start of the longest period
+                printf("Longest period end: %s %s\n", file_info[longest_start_index + longest_period_length - 1].date, file_info[longest_start_index + longest_period_length - 1].time);
 
                 break;
 
+            // Returns 0 and exits the program
             case 'Q':
             case 'q':
                 return 0;
                 break;
-        
+
+            // Prints an error message in case an invalid letter is selected for the menu
             default:
                 printf("Invalid choice. Try again\n");
                 break;
